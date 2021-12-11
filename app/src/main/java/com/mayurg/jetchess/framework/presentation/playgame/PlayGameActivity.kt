9f19c39eb0e29f66287c2990d0f740b88f085e15
Mayur Gajra <mayurgajra5@gmail.com>
2021-12-11 17:19:43 +0530

    - Player names textview added

--- app/src/main/java/com/mayurg/jetchess/framework/presentation/playgame/PlayGameActivity.kt
+++ app/src/main/java/com/mayurg/jetchess/framework/presentation/playgame/PlayGameActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.mayurg.jetchess.framework.presentation.base.BaseActivity
 import com.mayurg.jetchess.framework.presentation.playgame.boardview.BoardMainContainer
 import com.mayurg.jetchess.framework.presentation.playgame.gameview.Game
@@ -46,12 +51,14 @@ class PlayGameActivity : BaseActivity() {
                 }
 
                 Column {
+                    Text(text = "Player 1", color = Color.White, modifier = Modifier.padding(16.dp))
                     BoardMainContainer(
                         game = game,
                         selection = selection,
                         moves = game.movesForPieceAt(selection),
                         didTap = onSelect
                     )
+                    Text(text = "Player 2", color = Color.White, modifier = Modifier.padding(16.dp))
                 }
             }
         }
