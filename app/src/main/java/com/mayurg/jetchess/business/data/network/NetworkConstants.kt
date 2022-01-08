b375a348ed0e2569a04b80801e66e390572db074
Mayur Gajra <mayurgajra5@gmail.com>
2022-01-08 18:03:30 +0530

    Updated urls to new environment

--- app/src/main/java/com/mayurg/jetchess/business/data/network/NetworkConstants.kt
+++ app/src/main/java/com/mayurg/jetchess/business/data/network/NetworkConstants.kt
@@ -3,7 +3,7 @@ package com.mayurg.jetchess.business.data.network
 object NetworkConstants {
 
     const val NETWORK_TIMEOUT = 60000L
-    const val BASE_URL = "https://192.168.0.3:8081/"
-    const val WS_BASE_URL_LOCALHOST = "http://192.168.0.3:8081/ws/playGame"
+    const val BASE_URL = "http://192.168.0.3:8080/"
+    const val WS_BASE_URL_LOCALHOST = "http://192.168.0.3:8080/ws/playGame"
     const val RECONNECT_INTERVAL = 3000L
 }
